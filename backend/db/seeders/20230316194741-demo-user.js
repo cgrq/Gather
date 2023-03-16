aa694f7b3c0b8825ed635a6c55ac21c939e5aca8
Christian Rosa <crosa93921@gmail.com>
2023-03-16 13:07:49 -0700

    finished user model

---
+++ backend/db/seeders/20230316194741-demo-user.js
@@ -0,0 +1,28 @@
+'use strict';
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
+    return  queryInterface.bulkInsert(options, [
+      {
+        email: 'user1@gmail.com',
+        username: 'user1',
+        hashedPassword: bcrypt.hashSync('password')
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return  queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['user1'] }
+    }, {});
+  }
+};
