eaf8374263a564afca3f51636803267ac8dc8825
Christian Rosa <crosa93921@gmail.com>
2023-03-16 15:19:46 -0700

    Add User logout backend endpoint

--- backend/db/seeders/20230316194741-demo-user.js
+++ backend/db/seeders/20230316194741-demo-user.js
@@ -9,11 +9,21 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     options.tableName = 'Users';
-    return  queryInterface.bulkInsert(options, [
+    return queryInterface.bulkInsert(options, [
       {
-        email: 'user1@gmail.com',
-        username: 'user1',
+        email: 'demo@user.io',
+        username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user1@user.io',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'user2@user.io',
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password3')
       }
     ], {});
   },
@@ -21,8 +31,8 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     options.tableName = 'Users';
     const Op = Sequelize.Op;
-    return  queryInterface.bulkDelete(options, {
-      username: { [Op.in]: ['user1'] }
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
     }, {});
   }
 };
