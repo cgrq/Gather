fc3f5186b3ea7920bdcb71f28a751cbbdffcf221
Christian Rosa <crosa93921@gmail.com>
2023-04-13 07:48:50 -0700

    added link to sign up modal on click if user is not signed in

--- frontend/src/components/Home/SectionFour.js
+++ frontend/src/components/Home/SectionFour.js
@@ -1,7 +1,41 @@
-function SectionFour(){
-    return(
+import { useSelector } from 'react-redux';
+import React, { useState, useEffect, useRef } from "react";
+import OpenModalButton from '../OpenModalButton';
+import SignupFormModal from '../SignupFormModal';
+
+
+function SectionFour() {
+    const sessionUser = useSelector(state => state.session.user);
+    console.log(`🖥 ~ file: SectionFour.js:9 ~ SectionFour ~ sessionUser:`, sessionUser)
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef();
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = (e) => {
+          if (!ulRef.current.contains(e.target)) {
+            setShowMenu(false);
+          }
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu);
+      }, [showMenu]);
+
+      const closeMenu = () => setShowMenu(false);
+
+    return (
         <div className="section-four-container">
-            <button>Join Gather</button>
+            {
+                sessionUser
+                    ? <button>Join Gather</button>
+                    : <OpenModalButton
+                        buttonText="Join Gather"
+                        onButtonClick={closeMenu}
+                        modalComponent={<SignupFormModal />}/>
+            }
         </div>
     )
 }
