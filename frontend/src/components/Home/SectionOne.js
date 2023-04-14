fed4c8c2015291b765f37f28e772d53c2ecc9bdd
Christian Rosa <crosa93921@gmail.com>
2023-04-14 14:51:23 -0700

    finished preliminary icons, will be going back to flesh them out more

--- frontend/src/components/Home/SectionOne.js
+++ frontend/src/components/Home/SectionOne.js
@@ -2,10 +2,10 @@ function SectionOne(){
     return (
         <div className="section-one-container">
             <div>
-                <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+                <h1 className="home-h1">There's nothing like a gathering, so we made them our business.</h1>
                 <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
-            <img src="./section-one-infographic.png"/>
+            <img src="./landing-placeholder-main.png"/>
         </div>
     )
 }
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -11,7 +11,7 @@ function Navigation({ isLoaded }) {
     <div className="nav-list">
 
       <NavLink className="gather-title" exact to="/">
-        <img src={process.env.PUBLIC_URL + "/logo.png"} alt="Gather" />
+        <img className="gather-logo" src={process.env.PUBLIC_URL + "/logo.png"} alt="Gather" />
       </NavLink>
 
       {isLoaded && (
